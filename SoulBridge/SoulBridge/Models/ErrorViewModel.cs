150d4a29021236b41c81b6bd5dc210000783277b
S7venKing <112841486+S7venKing@users.noreply.github.com>
2024-06-03 18:33:44 +0700

    remove prj .NET

--- SoulBridge/SoulBridge/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SoulBridge.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
