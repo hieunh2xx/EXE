75fe4f6e7cee4e7720c9b01929c322194c31d654
hieunh2612 <hieunguyenhuy2001@gmail.com>
2024-06-04 11:55:39 +0700

    hieunh

---
+++ breath/script.js
@@ -0,0 +1,24 @@
+const textElement = document.querySelector(".text");
+const breathingSound = document.getElementById("breathing-sound");
+const audioControl = document.getElementById("audio-control");
+const audioIcon = audioControl.querySelector("i");
+
+function updateText() {
+  if (textElement.innerText === "Hít vào") {
+    textElement.innerText = "Thở ra";
+  } else {
+    textElement.innerText = "Hít vào";
+  }
+}
+
+audioControl.addEventListener("click", () => {
+  if (breathingSound.paused) {
+    breathingSound.play();
+    audioIcon.classList.remove("fa-volume-up");
+    audioIcon.classList.add("fa-volume-mute");
+  } else {
+    breathingSound.pause();
+    audioIcon.classList.remove("fa-volume-mute");
+    audioIcon.classList.add("fa-volume-up");
+  }
+});
